91f4133ca82d11c31817d0ad537f7924e7e754c0
rahmaw00 <ramata.will@gmail.com>
2018-04-30 22:25:27 +0000

    added button, can post

--- src/Spark.js
+++ src/Spark.js
@@ -4,34 +4,81 @@ import TextField from 'material-ui/TextField';
 import UserApi from './UserApi.js';
 import firebase from 'firebase';
 import { List, ListItem } from 'material-ui/List';
+import {GridList, GridTile} from 'material-ui/GridList';
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentAdd from 'material-ui/svg-icons/content/add';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+
+
+const ButtonStyle = {
+  position: "fixed",
+  right: 24,
+  bottom: 24,
+  marginBottom: 0,
+  zIndex: 998,
+};
 
 export default class Spark extends Component {
   constructor(props) {
     super(props);
+    var path = "/session/" + this.props.match.params.id;
+    this.sessionRef = firebase.database().ref(path);
+
     this.state = {
       posts: [],
       input: "",
+      open: false,
     };
+  }
+  handleOpen() {
+    this.setState({
+      open: true,
+    })
+  }
+  
+  handleClose() {
+    this.sessionRef.push({
+      user: "RW",
+      name:"Ramata",
+      image: this.state.input,
+    })
+    this.setState({
+      open: false,
+      input: "",
+    })
+  }
+
+  handleChange(event, newValue) {
+    this.setState({
+      input: newValue,
+    })
   }
 
   componentWillMount() {
-    this.setState({ posts: [
-        {
-            user: "RW",
-            name:"Ramata",
-            image: "https://www.funnypica.com/wp-content/uploads/2015/05/TOP-30-Cute-Cats-Cute-Cat-30.jpg",
-        },
-        {
-            user: "AC",
-            name:"Andrea",
-            image: "https://www.funnypica.com/wp-content/uploads/2015/05/TOP-30-Cute-Cats-Cute-Cat-30.jpg",
-        },
-           {
-            user: "AC",
-            name:"Andrea",
-            image: "https://www.funnypica.com/wp-content/uploads/2015/05/TOP-30-Cute-Cats-Cute-Cat-30.jpg",
-        }
-        ] });
+    
+    /*
+    From firebase: {
+    
+      "<random-id>": {
+        user: "foo",
+        name: "foo name",
+        image: "URL"
+      }
+      ....
+    }
+    
+    We want: [
+      {
+        user: "foo",
+        name: "foo name",
+        image: "URL"
+      }
+    ]
+    */
+    this.sessionRef.on("value", (snapshot) => this.setState({
+      posts: snapshot.val() ? Object.values(snapshot.val()) : [],
+    }))
   }
 
   componentWillUnmount() {
@@ -41,13 +88,43 @@ export default class Spark extends Component {
   }
 
   render() {
-    const posts = this.state.posts.map(post => {
-      return <img className="postImage" src={post.image}/>
-    })
+    const actions = [
+      <FlatButton
+        label="POST"
+        primary={true}
+        keyboardFocused={true}
+        onClick={() => this.handleClose()}
+      />,
+    ];
+
     return (
       <div style={{margin: 56}}>
-      {posts}
+      <Dialog
+          title="Paste your url..."
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+        >
+        <TextField onChange={(event, newValue) => this.handleChange(event, newValue)}
+              hintText="Paste your url"
+              floatingLabelText="Photo url" />        
+      </Dialog>
+        <GridList
+          cellHeight={"auto"}
+        >
+          {this.state.posts.map((tile) => (
+            <GridTile
+              title={tile.name}>            
+              <img src={tile.image} />
+            </GridTile>
+          ))}
+        </GridList>
+        <FloatingActionButton secondary={true} style={ButtonStyle} onClick={() => this.handleOpen()}>
+          <ContentAdd />
+        </FloatingActionButton>
       </div>
     );
   }
 }
+
+//subtitle={<span>by <b>{tile.author}</b></span>}
